9445bc2e7c057da83b5f74f81449cf5a83be6930
Edoardo Balducci <81587335+Bbalduzz@users.noreply.github.com>
2022-09-30 12:11:36 +0200

    Update main.py
    - change in headers due to riot changes

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def checker():
     headers = OrderedDict({
         "Accept-Language": "en-US,en;q=0.9",
         "Accept": "application/json, text/plain, */*",
-        'User-Agent': 'RiotClient/51.0.0.4429735.4381201 rso-auth (Windows;10;;Professional, x64)'
+        'User-Agent': 'RiotClient/58.0.0.4640299.4552318 rso-auth (Windows;10;;Professional, x64)'
     })
     session = sesh()
     session.headers = headers
@@ -35,7 +35,7 @@ def checker():
     }
     headers = {
         'Content-Type': 'application/json',
-        'User-Agent': 'RiotClient/51.0.0.4429735.4381201 rso-auth (Windows;10;;Professional, x64)',
+        'User-Agent': 'RiotClient/58.0.0.4640299.4552318 rso-auth (Windows;10;;Professional, x64)',
     }
     r = session.post(f'https://auth.riotgames.com/api/v1/authorization', json=data, headers=headers)
     data = {
@@ -56,7 +56,7 @@ def checker():
     else:
         print("Failed")
     headers = {
-        'User-Agent': 'RiotClient/51.0.0.4429735.4381201 rso-auth (Windows;10;;Professional, x64)',
+        'User-Agent': 'RiotClient/58.0.0.4640299.4552318 rso-auth (Windows;10;;Professional, x64)',
         'Authorization': f'Bearer {token}',
     }
     r = session.post('https://entitlements.auth.riotgames.com/api/token/v1', headers=headers, json={})
